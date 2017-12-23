e37d8b2d93ed29beee9146853224bf4083460a09
LoganS1 <rocket3701@yahoo.com>
2017-12-22 22:46:19 -0600

    Fix scaling on mobile screens

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var usernameInput = document.getElementById("username");
 var subtextInput = document.getElementById("subtext");
-var displayBanner = document.getElementById("banner");
+var banner = document.getElementById("banner");
 var generatorBTN = document.getElementById("generateBTN");
 var action = "https://generator.devbanner.center/banner?";
-
+var wrapper = document.getElementsByClassName("wrapper")[0];
+var backgroundImage = document.getElementsByClassName("background-image")[0];
+var content = document.getElementsByClassName("content")[0];
 /*
 Example URL
 https://generator.devbanner.center/banner?username=tisButABug&subtext=test
@@ -21,5 +23,18 @@ function generateBanner(){
   this.username = usernameInput.value;
   this.subtext = subtextInput.value;
   this.src = action + usernameInput.name + "=" + this.username + "&" + subtextInput.name + "=" + this.subtext;
-  displayBanner.src = this.src;
+  banner.src = this.src;
 }
+
+function resize() {
+  wrapper.height = window.innerHeight * 1.1;
+  backgroundImage.height = window.innerHeight * 1.2;
+  if(window.innerWidth < 600){
+    content.style.left = "45%";
+  }else{
+    content.style.left = "48%";
+  }
+}
+
+window.onresize = resize;
+resize();
